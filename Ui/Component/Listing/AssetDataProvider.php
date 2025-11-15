80543578b1a1976d31a4f3731a9e20fc583d4c6b
GARDENLAWN <rpcsky@gmail.com>
2025-11-15 17:13:42 +0100

    Init

--- Ui/Component/Listing/AssetDataProvider.php
+++ Ui/Component/Listing/AssetDataProvider.php
@@ -2,15 +2,20 @@
 namespace GardenLawn\MediaGallery\Ui\Component\Listing;
 
 use Magento\Ui\DataProvider\AbstractDataProvider;
-use GardenLawn\MediaGallery\Model\ResourceModel\Asset\CollectionFactory;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\App\RequestInterface;
 
 class AssetDataProvider extends AbstractDataProvider
 {
+    private ResourceConnection $resource;
+    private RequestInterface $request;
+
     /**
      * @param string $name
      * @param string $primaryFieldName
      * @param string $requestFieldName
-     * @param CollectionFactory $collectionFactory
+     * @param ResourceConnection $resource
+     * @param RequestInterface $request
      * @param array $meta
      * @param array $data
      */
@@ -18,11 +23,38 @@ class AssetDataProvider extends AbstractDataProvider
         $name,
         $primaryFieldName,
         $requestFieldName,
-        CollectionFactory $collectionFactory,
+        ResourceConnection $resource,
+        RequestInterface $request,
         array $meta = [],
         array $data = []
     ) {
-        $this->collection = $collectionFactory->create();
+        $this->resource = $resource;
+        $this->request = $request;
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+
+    /**
+     * Return data for UI listing in expected structure
+     *
+     * @return array
+     */
+    public function getData(): array
+    {
+        $connection = $this->resource->getConnection();
+        $assetTable = $connection->getTableName('media_gallery_asset');
+
+        $select = $connection->select()
+            ->from($assetTable, ['id', 'path', 'title']);
+
+        // Optional: if gallery_id provided, we still return all assets.
+        // Keeping logic simple to avoid heavy joins that could fail on misconfig.
+        // $galleryId = (int)($this->request->getParam('gallery_id') ?? 0);
+
+        $items = $connection->fetchAll($select);
+
+        return [
+            'totalRecords' => count($items),
+            'items' => array_values($items)
+        ];
     }
 }
