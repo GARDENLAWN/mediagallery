475437413b20eeac443922c956e847b6fead57f7
GARDENLAWN <rpcsky@gmail.com>
2025-11-15 11:57:26 +0100

    Init

--- Ui/Component/Listing/DataProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace GardenLawn\MediaGallery\Ui\Component\Listing;
-
-use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider as UiDataProvider;
-
-class DataProvider extends UiDataProvider
-{
-}
