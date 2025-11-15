ed1fa602a5417e5322fc718f4602b845344f86f9
GARDENLAWN <rpcsky@gmail.com>
2025-11-15 21:41:56 +0100

    Init

--- Model/ResourceModel/Gallery/Collection.php
+++ Model/ResourceModel/Gallery/Collection.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 namespace GardenLawn\MediaGallery\Model\ResourceModel\Gallery;
 
-use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult as UiSearchResult;
 use GardenLawn\MediaGallery\Model\Gallery;
 use GardenLawn\MediaGallery\Model\ResourceModel\Gallery as GalleryResource;
 
-class Collection extends AbstractCollection
+/**
+ * Grid collection that is compatible with Magento UI DataProvider
+ * by implementing SearchResultInterface via UiSearchResult base class.
+ */
+class Collection extends UiSearchResult
 {
     /**
      * @inheritDoc
@@ -19,6 +23,7 @@ class Collection extends AbstractCollection
      */
     protected function _construct(): void
     {
+        // Initialize model and resource; UiSearchResult will derive main table from the resource
         $this->_init(Gallery::class, GalleryResource::class);
     }
 }
