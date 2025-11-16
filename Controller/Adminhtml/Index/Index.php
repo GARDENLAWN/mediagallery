97af59a69f4261923bfd53332add54c980e7535c
GARDENLAWN <rpcsky@gmail.com>
2025-11-16 16:37:45 +0100

    Init

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -5,20 +5,12 @@ namespace GardenLawn\MediaGallery\Controller\Adminhtml\Index;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
-use Magento\Backend\Model\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
 {
-    /**
-     * @var PageFactory
-     */
     protected PageFactory $resultPageFactory;
 
-    /**
-     * @param Context $context
-     * @param PageFactory $resultPageFactory
-     */
     public function __construct(
         Context $context,
         PageFactory $resultPageFactory
@@ -30,14 +22,10 @@ class Index extends Action
     /**
      * Index action
      *
-     * @return Page
+     * @return \Magento\Framework\View\Result\Page
      */
-    public function execute(): Page
+    public function execute()
     {
-        /** @var Page $resultPage */
-        $resultPage = $this->resultPageFactory->create();
-        $resultPage->setActiveMenu('GardenLawn_MediaGallery::gallery');
-        $resultPage->getConfig()->getTitle()->prepend(__('Galleries'));
-        return $resultPage;
+        return $this->resultPageFactory->create();
     }
 }
