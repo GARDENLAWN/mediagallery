0208d4df7819f1c1da7427d1a1927e5178d0bc1a
GARDENLAWN <rpcsky@gmail.com>
2025-12-15 21:58:23 +0100

    Init

--- Console/Command/DeduplicateAssets.php
+++ Console/Command/DeduplicateAssets.php
@@ -11,7 +11,7 @@ use Psr\Log\LoggerInterface;
 
 class DeduplicateAssets extends Command
 {
-    const DRY_RUN_OPTION = 'dry-run';
+    const string DRY_RUN_OPTION = 'dry-run';
 
     protected ResourceConnection $resourceConnection;
     protected LoggerInterface $logger;
